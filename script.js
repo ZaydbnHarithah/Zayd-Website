c6c6c4d5aeab7c9265f8770c76c99539a3e292d6
ZaydbnHarithah Shareaa <zaydbnharithah@gmail.com>
2026-05-04 18:32:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// تفاعل القائمة في الهواتف
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    
+    if(burger) {
+        burger.addEventListener('click', () => {
+            nav.classList.toggle('nav-active');
+            burger.classList.toggle('toggle');
+        });
+    }
+}
+
+navSlide();
+
+// تأثير التمرير السلس (Smooth Scroll)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
